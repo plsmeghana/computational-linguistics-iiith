dec32eb2c5e6f6d7849f1483a93ce0fc23ffb0bc
plsmeghana <pmeghana99@gmail.com>
2020-07-19 14:23:46 +0530

    NEW FOLDER ADDED

---
+++ RDIP/Codes/exp.js
@@ -0,0 +1,7 @@
+function languagefun() {
+     document.getElementById('lang1').innerHTML =
+         ("Form a  sentence (Declarative or Interrogative or any other type) from the given words")
+     document.getElementById('lang2').innerHTML =
+         ("select the buttons in proper order")
+
+ } 
